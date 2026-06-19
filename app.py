bcfe7a1db7ac31c01d364f346e07cedba073687b
Malleddi Veera Venkata Nagendra <nagendramalleddi@gmail.com>
2026-06-19 22:13:48 +0530

    Change app to use dynamic port configuration
    Update app to run on dynamic port from environment variable.

--- app.py
+++ app.py
@@ -210,6 +210,9 @@ def logout():
     return redirect(url_for("login"))
 
 
+
+
 if __name__ == "__main__":
     init_db()
-    app.run(debug=False, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
